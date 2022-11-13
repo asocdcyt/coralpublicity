1b14f03f2ae3170b13d4f92438696f57e2d0d967
asocdcyt <115439521+asocdcyt@users.noreply.github.com>
2022-11-13 10:04:14 -0800

    ASDASDSADASDSADASD
    ASDASDAS

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ import {config} from 'dotenv';
 config();
 
 export default {
-    mongodbURL: process.env.MONGODB_URI || 'mongodb+srv://cool1:cool2@cluster0.5azzq.mongodb.net/dbCoralData',
+    mongodbURL: process.env.MONGODB_URI,
 };
